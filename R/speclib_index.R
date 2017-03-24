6cee62cfaddb37d92168f9805d27e21f22aa2a79
pat-s <patrick.schratz@gmail.com>
2017-03-24 12:56:33 +0100

    update to newest version

--- R/speclib_index.R
+++ R/speclib_index.R
@@ -72,3 +72,52 @@ setMethod("[", "Speclib",
     usagehistory(x) <- "Subsetting speclib (spectral and sample dimensions)"
   return(x)
 })
+
+
+setMethod("[", "Specfeat",
+          function(x, i, j, ...)
+{
+  x_new <- callNextMethod(x, i, j, ...)
+  FWL <- as.numeric(substr(names(x@features), 2, nchar(names(x@features))))
+  tmp <- x_new
+  class(tmp) <- "Speclib"
+  x_new_specfeat <- specfeat(tmp, FWL)
+  x_new@features      <- x_new_specfeat@features
+  x_new@featureLimits <- x_new_specfeat@featureLimits
+#   wl <- .get.feature.wavelength(x)
+#   rep <- .get.rep.feature.parts(wl, x)
+#   
+#   if (missing(i)) 
+#   {    
+#     for (k in 1:length(rep$matches))
+#         rep$matches[[k]] <- rep$matches[[k]][,j]     
+#   } else {
+#     if (missing(j))
+#     {
+#       for (k in 1:length(rep$matches))
+#         rep$matches[[k]] <- rep$matches[[k]][i,]          
+#     } else {
+#       for (k in 1:length(rep$matches))
+#         rep$matches[[k]] <- rep$matches[[k]][i,j]     
+#     }
+#   }
+#   for (m in 1:length(rep$matches))
+#   {
+#     for (k in 1:length(x@features[[i]]))
+#     {
+#       xval <- which(x@wavelength==x@features[[m]][[k]]$x1)
+#       xval <- x@wavelength[xval:(length(x@features[[m]][[k]]$y)+xval-1)]
+#       y    <- xval>=limits[2*(m-1)+1] & xval<=limits[2*(m-1)+2]
+#       
+#       if (any(y))
+#       {
+#         x_new@features[[m]][[k]]$y  <- x@features[[m]][[k]]$y[y]
+#         x_new@features[[m]][[k]]$x1 <- xval[y][1]
+#       } else {
+#         x_new@features[[m]][[k]]$y  <- NaN
+#         x_new@features[[m]][[k]]$x1 <- limits[2*(m-1)+1]
+#       }      
+#     }
+#   }
+  return(x_new)
+})
